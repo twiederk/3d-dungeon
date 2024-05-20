c074ddb979a7d8e3881f49b99d918560e203fcc1
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-05-20 09:44:53 +0200

    Implemented hext_to_coords

--- Overview.gd
+++ Overview.gd
@@ -1,14 +1,19 @@
 class_name Overview
 extends Node2D
 
+const hex_to_coords_mapping = {
+	0: Vector2(5, 2)
+}
+
+
 func _ready():
 	var map = load_map()
 	Globals.map = map
 	generate_map(map)
-	
-	
+
+
 func load_map() -> Array:
-	var allthelines = []        
+	var allthelines = []
 
 	var file = FileAccess.open("res://maps/map3.txt", FileAccess.READ)
 	while !file.eof_reached():
@@ -17,7 +22,8 @@ func load_map() -> Array:
 		allthelines.append(line)
 	file.close()
 	return allthelines
-	
+
+
 func generate_map(map: Array):
 	for y in map.size():
 		for x in range(map[0].length()):
@@ -25,3 +31,7 @@ func generate_map(map: Array):
 			# conver walls.hex() to vector in atlas
 			# set tile on tilemap
 			# tilemap.set_cell(TILE_SET_LAYER_GROUND, map_position, TILE_SET_SOURCE_ID, TILE_FENCE)
+
+
+func hex_to_coords(hex: int) -> Vector2:
+	return hex_to_coords_mapping[hex]
--- test/test_Overview.gd
+++ test/test_Overview.gd
@@ -2,18 +2,20 @@ extends GutTest
 
 var overview: Overview = null
 
+
 func before_each():
 	overview = Overview.new()
 
 
 func after_each():
 	overview.free()
-	
+
 
 func test_should_cool_down_when_weapon_is_hot():
 	# arrange
 
 	# act
+	var coords = overview.hex_to_coords(0)
 
 	# assert
-	assert_true(true)
+	assert_eq(coords, Vector2(5, 2))
