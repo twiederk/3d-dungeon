849b6ae7596ac181afd4f64faa1e4f6d9f301409
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-05-23 06:22:40 +0100

    Removed unused function from Cell

--- Cell.gd
+++ Cell.gd
@@ -7,15 +7,3 @@ extends Node3D
 @onready var southFace: = $SouthFace
 @onready var westFace: = $WestFace
 @onready var bottomFace: = $BottomFace
-
-
-func update_faces(cell_list) -> void:
-	var my_grid_position = Vector2(position.x / Globals.GRID_SIZE, position.z / Globals.GRID_SIZE)
-	if cell_list.has(my_grid_position + Vector2.RIGHT):
-		eastFace.queue_free()
-	if cell_list.has(my_grid_position + Vector2.LEFT):
-		westFace.queue_free()
-	if cell_list.has(my_grid_position + Vector2.DOWN):
-		southFace.queue_free()
-	if cell_list.has(my_grid_position + Vector2.UP):
-		northFace.queue_free()
